a02122a2a5642e1e01ec941cad5c5f770138a172
vamshi <vamshi.bolishetti@gmail.com>
2022-02-19 18:21:03 +0000

    logout functionality

---
+++ src/main/java/flipkart/logout.java
@@ -0,0 +1,13 @@
+package flipkart;
+
+/**
+ * Hello world!
+ *
+ */
+public class logout 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/flipkart/logoutTest.java
@@ -0,0 +1,20 @@
+package flipkart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class logoutTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
