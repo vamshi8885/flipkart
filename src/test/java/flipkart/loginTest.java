21bfa81959499c15f96258c5ebabdbeaba141e53
vamshi <vamshi.bolishetti@gmail.com>
2022-02-19 13:13:04 +0000

    login functionality

---
+++ src/test/java/flipkart/loginTest.java
@@ -0,0 +1,20 @@
+package flipkart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class loginTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
